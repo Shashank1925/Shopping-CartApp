26b774bc69336bba609062c412d7f4f18fef215b
Shashank Negi <shashanksingh.1728@gmail.com>
2025-03-21 11:16:25 +0530

    gmail and password change

--- src/order/order.Route.js
+++ src/order/order.Route.js
@@ -6,5 +6,6 @@ orderRouter.post("/new", jwtAuthentication, placeOrderController);
 orderRouter.get("/:orderId", jwtAuthentication, getOrderController);
 orderRouter.get("/my/orders", jwtAuthentication, getAllOrdersController);
 orderRouter.get("/orders/placed", jwtAuthentication, getAllUsersOrdersController);
+// Always put dynamic routes of same method at the bottom as  they are more specific than static routes and will cause conflicts if placed above static routes
 orderRouter.put("/update/:orderId", jwtAuthentication, updateOrderStatusController);
 export default orderRouter;
